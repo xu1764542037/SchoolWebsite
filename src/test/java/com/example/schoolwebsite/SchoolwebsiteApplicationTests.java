314ef3e51adaeee22c99f1940d51d494ec73d01f
Meteorshower22 <1621483161@qq.com>
2021-09-10 11:09:12 +0800

    管理员模块完成，未测试

--- src/test/java/com/example/schoolwebsite/SchoolwebsiteApplicationTests.java
+++ src/test/java/com/example/schoolwebsite/SchoolwebsiteApplicationTests.java
@@ -1,11 +1,10 @@
 package com.example.schoolwebsite;
 
-import com.example.schoolwebsite.entity.*;
 import com.example.schoolwebsite.entity.Class;
-import com.example.schoolwebsite.service.impl.GradeServiceImpl;
-import com.example.schoolwebsite.service.impl.ProfessionServiceImpl;
-import com.example.schoolwebsite.service.impl.StudentServiceImpl;
-import com.example.schoolwebsite.service.impl.TeacherServiceImpl;
+import com.example.schoolwebsite.entity.Profession;
+import com.example.schoolwebsite.entity.UserInfo;
+import com.example.schoolwebsite.service.impl.*;
+import com.example.schoolwebsite.utils.AdminLoginRandom;
 import com.example.schoolwebsite.utils.IdMaker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +17,16 @@ class SchoolwebsiteApplicationTests {
     GradeServiceImpl gradeService;
     @Autowired
     ProfessionServiceImpl professionService;
-
+    @Autowired
+    TeacherServiceImpl teacherService;
+    @Autowired
+    ClassServiceImpl classService;
+    @Autowired
+    UserInfoServiceImpl userInfoService;
 
     @Test
     void contextLoads(){
-        Grade grade = new Grade();
-        grade.setStatus("正常");
-        grade.setId(20451573);
-        grade.setGrade(100);
-//        System.out.println(gradeService.select("李四",null));
-        System.out.println(professionService.select(null,202112));
+        System.out.println(AdminLoginRandom.LoginRanDom());
     }
     @Test
     void tests(){
